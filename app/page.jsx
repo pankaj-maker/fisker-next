f787d2623a4b119a9d84fb321f07319c93c194a8
pankaj-maker <Pankaj14321@protonmail.com>
2024-12-04 07:48:46 +0000

    preview component complete

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
 import Header from "@/components/Header";
-import Head from "next/head";
-import Image from "next/image";
+import Preview from "@/components/Preview";
+import Configurator from "@/components/Configurator";
+
+
 function HomePage() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main className="flex flex-col sm:flex-row justify-between md:gap-8 px-2 md:px-8 py-3 md:py-6">
+        <section className="w-full md:w-3/4">
+          <Preview />
+        </section>
+        <aside className="w-full md:w-1/4">
+          <Configurator />
+        </aside>
+      </main>
+    </>
   );
 }
 export default HomePage;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,25 +1,30 @@
 import Image from "next/image";
+
 function Header() {
   return (
-    <div className="flex flex-col  h-screen">
-    <p className="text-1xl text-center font-semibold py-2">
-      All electric • Dynamic • Driver Range
-    </p>
-    <header className="flex justify-between py-3 px-">
-      <Image
-        src="fisker.svg"
-        height={80}
-        width={80}
-        alt="fisker"
-        className="w-12 sm:w-16 h-auto cursor-pointer hover:scale-110
-        transition-transform duration-100"
-      />
-      <span className="flex gap-2 items-center">
-      <Image src="globe.svg" height={20} width={20} alt="globe"  />
-      <p className="text-sm sm:text-base"></p>
-      </span>
-    </header>
-  </div>
+    <div className="text-stone-900">
+      {/* Header */}
+      <div className="bg-stone-100">
+        <p className="text-center font-semibold py-2">
+          All electric &#xb7; Dynamic &#xb7; Driver Range
+        </p>
+      </div>
+      <header className="flex justify-between px-10 py-3">
+        <Image
+          src="fisker.svg"
+          width={100}
+          height={100}
+          alt="Fisker Logo"
+          className="w-12 sm:w-16 h-auto cursor-pointer hover:scale-110 transition-transform duration-100"
+        />
+        {/* Region */}
+        <a href="#" className="flex gap-2 items-center">
+          <Image src="globe.svg" width={20} height={20} alt="Globe Icon" />
+          <p className="text-sm sm:text-base">US</p>
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default Header;
